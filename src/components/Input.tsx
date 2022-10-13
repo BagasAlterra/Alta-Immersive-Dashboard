f02f34f80debb90dc78c5be320d1fee8058bf11a
bagasalterra <bagas.dhitya@alterra.id>
2022-10-13 09:46:04 +0700

    restructuring input text

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,70 +4,42 @@ interface Props {
     size: string,
     outline: boolean,
     id?: string,
-    placeholder: string,
-    type: string,
+    type?: string,
+    placeholder?: string,
+    value?: string,
+    name?: string,
     onChange?: ChangeEventHandler
 }
 
-const Input: FC<Props> = ({ size, placeholder, type, id, outline, onChange }) => {
+const Input: FC<Props> = ({ size, value, outline, type, name, id, placeholder, onChange }) => {
     return (
         <>
-            {
-                outline === true ?
-                    <>
-                        {
-                            size === "long" ?
-                                <input onChange={onChange} type={type} id={id} className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-md h-9 w-5/6 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                :
-                                <>
-                                    {
-                                        size === "medium" ?
-                                            <input onChange={onChange} type={type} id={id} className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-md h-9 w-1/3 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                            :
-                                            <>
-                                                {
-                                                    size === "small" ?
-                                                        <input onChange={onChange} type={type} id={id} className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-md h-9 w-1/5 pl-2" tabIndex={-1} placeholder={placeholder}></input> :
-                                                        <>
-                                                            {
-                                                                size === "xtra-small" &&
-                                                                <input onChange={onChange} type={type} id={id} className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-md h-9 w-1/6 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                                            }
-                                                        </>
-                                                }
-                                            </>
-                                    }
-                                </>
+            <input
+                id={id}
+                className={
+                    outline === true ?
+                        `  <>
+                            ${size === "long" ? "bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded h-9 w-5/6 pl-2" :
+                            size === "medium" ? "bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded h-9 w-1/3 pl-2" :
+                                size === "small" ? "bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded h-9 w-1/5 pl-2" :
+                                    size === "xtra-small" && "bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded h-9 w-1/6 pl-2"
                         }
-                    </>
-                    :
-                    <>
-                        {
-                            size === "long" ?
-                                <input onChange={onChange} type={type} id={id} className="bg-gray-50 text-gray-900 text-sm rounded-md h-9 w-5/6 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                :
-                                <>
-                                    {
-                                        size === "medium" ?
-                                            <input onChange={onChange} type={type} id={id} className="bg-gray-50 text-gray-900 text-sm rounded-md h-9 w-1/3 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                            :
-                                            <>
-                                                {
-                                                    size === "small" ?
-                                                        <input onChange={onChange} type={type} id={id} className="bg-gray-50 text-gray-900 text-sm rounded-md h-9 w-1/5 pl-2" tabIndex={-1} placeholder={placeholder}></input> :
-                                                        <>
-                                                            {
-                                                                size === "xtra-small" &&
-                                                                <input onChange={onChange} type={type} id={id} className="bg-gray-50 text-gray-900 text-sm rounded-md h-9 w-1/6 pl-2" tabIndex={-1} placeholder={placeholder}></input>
-                                                            }
-                                                        </>
-                                                }
-                                            </>
-                                    }
-                                </>
+                           </>`
+                        :
+                        `  <>
+                        ${size === "long" ? "bg-gray-50  text-gray-900 text-sm rounded h-9 w-5/6 pl-2" :
+                            size === "medium" ? "bg-gray-50  text-gray-900 text-sm rounded h-9 w-1/3 pl-2" :
+                                size === "small" ? "bg-gray-50  text-gray-900 text-sm rounded h-9 w-1/5 pl-2" :
+                                    size === "xtra-small" && "bg-gray-50  text-gray-900 text-sm rounded h-9 w-1/6 pl-2"
                         }
-                    </>
-            }
+                           </>`
+                }
+                placeholder={placeholder}
+                type={type}
+                value={value}
+                name={name}
+                onChange={onChange}
+            ></input>
         </>
     )
 }
