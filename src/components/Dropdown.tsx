d74d480392e0bd341cf5b5777dbe0d9feba3f949
bagasalterra <bagas.dhitya@alterra.id>
2022-10-18 09:26:54 +0700

    refactoring component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react'
 
 interface Props {
-    children?: React.ReactNode;
+    data?: any[]
     title: string
 }
 
-const Dropdown: FC<Props> = ({ children, title }) => {
+const Dropdown: FC<Props> = ({ data, title }) => {
     return (
-        <>
-            <select className="rounded-md select-md w-1/3 pl-2 bg-gray-50 border border-gray-400 text-sm m-1 w-1/5 ">
-                <option disabled selected>{title}</option>
-                {children}
-            </select>
-        </>
+        <select className="rounded-md select-md w-1/3 pl-2 bg-gray-50 border border-gray-400 text-sm m-1 w-1/5 ">
+            <option disabled selected>{title}</option>
+            {data ? data.map((item) => (
+                <option value={item.value}>{item.label}</option>
+            )) :
+                <option selected>No data available</option>
+            }
+        </select>
     )
 }
 
--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -10,20 +10,18 @@ interface Props {
 
 const RadioButton: FC<Props> = ({ id, checked, value, name, onChange }) => {
     return (
-        <>
-            <div className='flex'>
-                <label className='label'>
-                    <input id={id} onChange={onChange} type="radio" name={name}
-                        className={
-                            "radio radio-md checked:" + (checked ? 'bg-alta-orange' : 'border border-alta-orange')
-                        }
-                        value={value}
-                        checked={checked === true ? true : false}
-                    />
-                    <span className="label-text ml-4">{name}</span>
-                </label>
-            </div>
-        </>
+        <div className='flex'>
+            <label className='label'>
+                <input id={id} onChange={onChange} type="radio" name={name}
+                    className={
+                        "radio radio-md checked:" + (checked ? 'bg-alta-orange' : 'border border-alta-orange')
+                    }
+                    value={value}
+                    checked={checked === true ? true : false}
+                />
+                <span className="label-text ml-4">{name}</span>
+            </label>
+        </div>
     )
 }
 
