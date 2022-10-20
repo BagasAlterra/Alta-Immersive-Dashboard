c82f5cf3d73871e75ffe3d23226ce96a6feacdf3
bagasalterra <bagas.dhitya@alterra.id>
2022-10-20 18:02:22 +0700

    log section responsivity

--- src/components/LogSection.tsx
+++ src/components/LogSection.tsx
@@ -10,13 +10,15 @@ interface Props {
 
 const LogSection: FC<Props> = ({ section, name, date, status, feedback }) => {
     return (
-        <div className="card w-120 h-72 flex-row justify-between bg-white shadow">
+        <div className="card justify-between bg-white shadow
+         lg:w-120 lg:h-72 md:w-120 md:h-72 w-30
+         lg:flex-row md:flex-row sm:flex-row ">
             <div className="m-5">
                 <h2 className="card-title font-inter font-semibold mb-2 text-black">{section}</h2>
                 <p className="font-inter font-medium mb-2 text-black">{name}</p>
                 <p className='font-inter font-thin text-black'>{date}</p>
             </div>
-            <div className="m-7 w-96 break-all">
+            <div className=" break-all lg:m-7 lg:w-96 md:m-7 md:w-96 sm:m-7 sm:w-96 m-7">
                 <p className="font-inter font-normal mb-2 text-black">{feedback}</p>
                 <p className='font-inter font-semibold text-black'>Changed Status : {status ? status : "-"}</p>
             </div>
