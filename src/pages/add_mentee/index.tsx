3f98028b521c80f9258bda015a89f0741a9afb3c
BagasAlterra <bagas.dhitya@alterra.id>
2022-11-18 08:19:26 +0700

    react hook form integrated

--- src/pages/add_mentee/index.tsx
+++ src/pages/add_mentee/index.tsx
@@ -9,25 +9,25 @@ import Button from 'components/Button'
 
 const addMentee = () => {
 
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = (data: any) => {
+        alert(JSON.stringify(data))
+    }
+
     return (
         <Layout subTitle="Add New">
-            <form >
+            <form onSubmit={handleSubmit(onSubmit)} >
                 <div className="rounded shadow-lg bg-white w-full">
                     <div className='p-5'>
                         <p className='my-3 text-black'>Name</p>
-                        <Input
-                            id={'name'}
-                            name={'name'}
+                        <Input label='Name' id={'name'} name={'name'} register={register} required
                         />
                         <p className='my-3 text-black'>Address</p>
-                        <Input
-                            id={'address'}
-                            name={'address'}
+                        <Input label='Address' id={'address'} name={'address'} register={register} required
                         />
                         <p className='my-3 text-black'>Home Address</p>
-                        <Input
-                            id={'home_address'}
-                            name={'home_address'}
+                        <Input label='Home Address' id={'home_address'} name={'home_address'} register={register} required
                         />
                         <div className='my-3 flex'>
                             <p className='text-black mt-2'>Gender</p>
@@ -39,32 +39,22 @@ const addMentee = () => {
                             </div>
                         </div>
                         <p className='my-3 text-black'>Email</p>
-                        <Input
-                            id={'email'}
-                            name={'email'}
+                        <Input label='Email' id={'email'} name={'email'} register={register} required
                         />
                         <p className='my-3 text-black'>Telegram</p>
-                        <Input
-                            id={'telegram'}
-                            name={'telegram'}
+                        <Input label='Telegram' id={'telegram'} name={'telegram'} register={register} required
                         />
                         <p className='my-3 text-black'>Phone</p>
-                        <Input
-                            id={'phone'}
-                            name={'phone'}
+                        <Input label='Phone' id={'phone'} name={'phone'} register={register} required
                         />
                     </div>
                     <div className='p-5'>
                         <p className='text-black text-lg font-bold'>Emergency</p>
                         <p className='my-3 text-black'>Name</p>
-                        <Input
-                            id={'emergency_name'}
-                            name={'emergency_name'}
+                        <Input label='Emergency Name' id={'emergency_name'} name={'emergency_name'} register={register} required
                         />
                         <p className='my-3 text-black'>Phone</p>
-                        <Input
-                            id={'emergency_phone'}
-                            name={'emergency_phone'}
+                        <Input label='Emergency Phone' id={'emergency_phone'} name={'emergency_phone'} register={register} required
                         />
                         <p className='my-3 text-black'>Status</p>
                         <Dropdown title='status' />
@@ -81,27 +71,19 @@ const addMentee = () => {
                             </div>
                         </div>
                         <p className='my-3 text-black'>Major</p>
-                        <Input
-                            id={'major'}
-                            name={'major'}
+                        <Input label='Major' id={'major'} name={'major'} register={register} required
                         />
                         <p className='my-3 text-black'>Graduate</p>
-                        <Input
-                            id={'graduate'}
-                            name={'graduate'}
+                        <Input label='Graduate' id={'graduate'} name={'graduate'} register={register} required
                         />
                     </div>
                     <div className='p-5 flex flex-row-reverse'>
                         <div>
-                            <Button
-                                label='Save'
-                                id='save'
+                            <Button type='submit' label='Save' id='save'
                             />
                         </div>
                         <div className='mr-5'>
-                            <Button
-                                label='Cancel'
-                                id='cancel'
+                            <Button variant='secondary' label='Cancel' id='cancel'
                             />
                         </div>
                     </div>
